b11a9c7d42d6d971b5c764c21fb9cbe348c2b62b
NAVEEN <NAVEEN@LAPTOP-MV04GFAT>
2024-11-20 17:14:15 +0530

    second commit

---
+++ Sample/src/Sheet/RowCellCount.java
@@ -0,0 +1,15 @@
+package Sheet;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class RowCellCount {
+
+	public static void main(String[] args) throws Throwable {
+		
+		FileInputStream fi=new FileInputStream("C:\\files.xlsx");
+		
+
+	}
+
+}
